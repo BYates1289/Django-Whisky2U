b5d7dccc6623f1a0ff3ffa4621f441af5660e4c4
BYates1289 <benyates1289@live.com>
2023-03-27 12:27:07 +0500

    Update settings.py

--- whiskey_me/settings.py
+++ whiskey_me/settings.py
@@ -79,22 +79,22 @@ WSGI_APPLICATION = "whiskey_me.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#    "default": {
-#        "ENGINE": "django.db.backends.sqlite3",
-#        "NAME": BASE_DIR / "db.sqlite3",
-#    }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "d7n1u54hndim9c",
-        "USER": os.environ['DB_USER'],
-        "PASSWORD": os.environ['DB_PASSWORD'],
-        "HOST": "ec2-52-49-56-163.eu-west-1.compute.amazonaws.com",
-        "PORT": "5432"
-    }}
+   "default": {
+       "ENGINE": "django.db.backends.sqlite3",
+       "NAME": BASE_DIR / "db.sqlite3",
+   }
+}
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "d7n1u54hndim9c",
+#         "USER": os.environ['DB_USER'],
+#         "PASSWORD": os.environ['DB_PASSWORD'],
+#         "HOST": "ec2-52-49-56-163.eu-west-1.compute.amazonaws.com",
+#         "PORT": "5432"
+#     }}
 
 
 # Password validation
