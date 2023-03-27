479dc2a8ac02376e4ee2fa28d997e49a3b1f42f8
BYates1289 <benyates1289@live.com>
2023-03-27 15:19:59 +0500

    Update wsgi.py

--- whiskey_me/wsgi.py
+++ whiskey_me/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+from dotenv import load_dotenv
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'whiskey_me.settings')
+project_folder = '/home/whiskyme/Django-Whisky2U'
+load_dotenv(os.path.join(project_folder, '.env'))
 
 application = get_wsgi_application()
